2fd17816f6fc3ca207b0d2775f6632a1838e6a2c
DiegoGuedes56 <oiguedes2010@hotmail.com>
2022-10-15 17:19:22 -0300

    mapadev

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* 
+---- passo 3 - quando o usuario clicar, desmarcar a aba selecionada.
+---- passo 4 - marcar a aba clicada como selecionado.
+---- passo 5 - esconder o conteudo anterior. 
+---- passo 6 - mostrar o conteudo da aba selecionada.  
+*/
+
+console.log document.querySelectorAll(".aba");
